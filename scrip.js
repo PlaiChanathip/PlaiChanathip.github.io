eeb378624c532591d2938f3f44553ece8a367dd7
CHANATHIP HWANGPERMPUL <s6401072510079@kmutnb.ac.th>
2023-10-10 01:15:37 +0700

    First commit

---
+++ scrip.js
@@ -0,0 +1,37 @@
+var facebookIcon = document.getElementById("facebook-icon");
+facebookIcon.addEventListener("click", function() {
+    window.open( "https://www.facebook.com/plaiilililil/", "_blank");
+
+
+
+
+});
+
+
+var lineIcon = document.getElementById("line-icon");
+lineIcon.addEventListener("click", function() {
+    window.open( "https://line.me/ti/p/mRQVSApZxr", "_blank");
+});
+
+var instagramIcon = document.getElementById("instagram-icon");
+instagramIcon.addEventListener("click", function() {
+    window.open( "https://instagram.com/plaite__en?igshid=OGQ5ZDc2ODk2ZA==", "_blank");
+    
+});
+
+var githubIcon = document.getElementById("github-icon");
+githubIcon.addEventListener("click", function() {
+    window.open( "https://www.youtube.com/", "_blank");
+    
+});
+
+
+var discordIcon = document.getElementById("discord-icon");
+discordIcon.addEventListener("click", function() {
+    window.open( "https://discord.gg/vahP4Cyn", "_blank");
+    
+});
+
+
+
+
